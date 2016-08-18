80f5b1c46c50924b1865ec8946c10f5bf4138f1e
Greg Hill <greg.hill@rackspace.com>
2016-08-18 09:53:22 -0500

    Add CORS support to the API so the UI can use it

--- api/swd6/api/app.py
+++ api/swd6/api/app.py
@@ -1,5 +1,6 @@
-from sqlalchemy_jsonapi import flaskext as flask_jsonapi
 import flask
+import flask_cors
+from sqlalchemy_jsonapi import flaskext as flask_jsonapi
 import logging
 
 from swd6.config import CONF
@@ -8,11 +9,16 @@ from swd6.db.models import db
 logging.basicConfig(level=logging.DEBUG)
 
 app = flask.Flask(__name__)
+app.config['DEBUG'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = CONF.db.uri
 app.config['SERVER_NAME'] = CONF.api.host
 
 app.logger.setLevel(logging.DEBUG)
+
+flask_cors.CORS(app, origins=CONF.api.cors_hosts)
+
+logging.getLogger('flask_cors').level = logging.DEBUG
 
 db.init_app(app)
 
