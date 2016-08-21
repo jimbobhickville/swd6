739522cdc420092fde2fb91f3a05c42f95a8c345
caiges <caigesn@gmail.com>
2016-08-20 22:18:59 -0700

    Pull in redux logger so we can follow state in dev-tools

--- ui-react/src/js/main.jsx
+++ ui-react/src/js/main.jsx
@@ -6,17 +6,23 @@ import { Provider } from 'react-redux';
 import { Router, Route, browserHistory } from 'react-router';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { reducer as api } from 'redux-json-api';
-import thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 
 import { About } from './about';
 import { App } from './app';
 import { Home } from './home';
 import { RacePage } from './races';
 
+const loggerMiddleware = createLogger();
+
 const reducers = combineReducers({
   api
 });
-let store = createStore(reducers, applyMiddleware(thunk));
+let store = createStore(reducers, applyMiddleware(
+  thunkMiddleware, // lets us dispatch() functions
+  loggerMiddleware // neat middleware that logs actions
+));
 
 let root = document.createElement('div');
 root.setAttribute('id', 'app');
