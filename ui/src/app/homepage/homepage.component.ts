a7b6d2e085fa806bf2da9539ef562cac66265a8e
Greg Hill <greg.hill@rackspace.com>
2016-08-25 09:44:57 -0500

    Remove abandoned Angular 2 UI

--- ui/src/app/homepage/homepage.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  moduleId: module.id,
-  templateUrl: 'homepage.component.html'
-})
-export class HomepageComponent {
-  title: 'Welcome!'
-}
--- ui/src/app/races/race.ts
+++
@@ -1,3 +0,0 @@
-export class Race {
-  name: string
-}
