3dcffd5c1abf92c5bdbcda3277d20849394a4556
Greg Hill <jimbobhickville@gmail.com>
2020-12-18 13:22:27 -0600

    Update sql for latest postgres
    I guess these auto-generated statements are incompatible
    with the latest pg container image. We don't need them.

--- pgdata/initial.sql
+++ pgdata/initial.sql
@@ -4583,16 +4583,6 @@ ALTER TABLE ONLY weapon_ranged_image
 
 
 --
--- Name: public; Type: ACL; Schema: -; Owner: postgres
---
-
-REVOKE ALL ON SCHEMA public FROM PUBLIC;
-REVOKE ALL ON SCHEMA public FROM postgres;
-GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL ON SCHEMA public TO PUBLIC;
-
-
---
 -- PostgreSQL database dump complete
 --
 
